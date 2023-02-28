468742c1ba0fcf0ddcc8350f582f7d832266faaf
Raeki <mkeithley@gmail.com>
2023-02-28 21:43:29 +0900

    tautological general::general - per cargo clippy

--- src/bin/rush/general.rs
+++ src/bin/rush/general.rs
@@ -1,11 +1,11 @@
+extern crate libc;
+use std::fs::File;
+use std::os::unix::fs::OpenOptionsExt;
 
-pub mod general {
-
-  extern crate libc;
-  use std::fs::File;
-  use std::os::unix::fs::OpenOptionsExt;
-
-  pub fn check_dev_tty() {
-    let mut _tty = File::options().read(true).write(true).custom_flags(libc::O_NONBLOCK).open("/dev/tty");
-  }
+pub fn check_dev_tty() {
+    let mut _tty = File::options()
+        .read(true)
+        .write(true)
+        .custom_flags(libc::O_NONBLOCK)
+        .open("/dev/tty");
 }
